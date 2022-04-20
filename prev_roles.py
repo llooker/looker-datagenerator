4c7980757372db05d97dd75fbbb3dffd7a14af13
Elliot Glasenk <eglasenk@google.com>
2022-04-20 19:08:15 +0000

    Fix issue with prev roles title formatting

--- prev_roles.py
+++ prev_roles.py
@@ -2,11 +2,11 @@ import csv
 import datetime
 import random
 titles_by_org = {
-    "sales": ["AE", "SE", "FSR"],
-    "marketing": ["Marketing Analyst", "Marketing Manager"],
-    "engineering": ["SWE", "Data Eng"],
-    "product": ["PM", "UI", "UX"],
-    "customer_success": ["CSM", "CSE", "TSM"],
+    "Sales": ["AE", "SE", "FSR"],
+    "Marketing": ["Marketing Analyst", "Marketing Manager"],
+    "Engineering": ["SWE", "Data Eng"],
+    "Product": ["PM", "UI", "UX"],
+    "Customer Success": ["CSM", "CSE", "TSM"],
 }
 
 today = datetime.date.today()
@@ -21,5 +21,5 @@ with open('data/employees.csv', 'r+', newline='') as obj:
                                 row["hired_date"], '%Y-%m-%d').date())).days
         if days_since_hired / 365 > 6:
             row["title"] = random.choices(
-                [i for i in titles_by_org[row["org"].lower().replace(' ', '_')] if i != row["org"]])[0]
+                [i for i in titles_by_org[row["org"]] if i != row["org"]])[0]
             dict_writer.writerow(row.values())
