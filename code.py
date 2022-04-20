6bc30bb7ece11b32decdf4621cdc056b37695522
Elliot Glasenk <eglasenk@google.com>
2022-04-20 18:50:42 +0000

    Finalize dynamic title logic

--- code.py
+++ code.py
@@ -13,7 +13,7 @@ class Table:
         for key, value in tables[table].items():
             if "primary_key" in value:
                 output += f"\t{key}: {value['type']} = dataclasses.field(default_factory=itertools.count(start=1).__next__)\n"
-            elif "exclude_from_output" not in value:
+            else:
                 output += f"\t{key}: {value['type']} = dataclasses.field(init=False)\n"
         return output
 
@@ -36,7 +36,7 @@ class Table:
         output = ""
         dependents = dict()
         for key, value in tables[table].items():
-            if "primary_key" not in value or "exclude_from_output" not in value:
+            if "primary_key" not in value:
                 if self.helper_parse_values(key, value, tables[table]) is not None:
                     output += self.helper_parse_values(key,
                                                        value, tables[table])
@@ -50,9 +50,9 @@ class Table:
         tmp_out = "\t\treturn f'"
         keys = list(tables[table].keys())
 
-        for key in keys[:-2]:
+        for key in keys[:-1]:
             tmp_out += f"{{self.{key}}},"
-        tmp_out += f"{{self.{keys[-2]}}}'\n"  # lol
+        tmp_out += f"{{self.{keys[-1]}}}'\n"  # lol
         output += tmp_out
         return output
 
@@ -65,47 +65,44 @@ class Table:
 
     def helper_parse_values(self, key, value, table):
         output = ""
-        if "exclude_from_output" not in value:
-            if value["type"] == "datetime" and "min_date" in value:
-                date = value["min_date"]
-                return f"\t\tself.{key} = self.created_at(datetime.datetime({date['year']}, {date['month']}, {date['day']}))\n"
-            elif all(k in value for k in ("values", "distribution")):
-                return f"\t\tself.{key} = self.random_item(population={value['values']}, distribution={value['distribution']})\n"
-            elif "values" in value:
-                return f"\t\tself.{key} = self.random_item(population={value['values']})\n"
-            elif key == "last_name":
-                return f"\t\tself.{key} = fake.last_name_nonbinary()\n"
-            elif key == "email" and "domain" in value:
-                return f"\t\tself.{key} = f'{{self.first_name.lower()}}{{self.last_name.lower()}}@{value['domain']}'\n"
-            elif key == "email" and "domain" not in value:
-                return f"\t\tself.{key} = f'{{self.first_name.lower()}}{{self.last_name.lower()}}@{{fake.safe_domain_name()}}'\n"
-            elif "depends_on" in value:
-                # single dependency
-                if len(value["depends_on"]) == 1 and value["depends_on"][0] == "gender":
-                    dependent_var = value["depends_on"][0]
-                    for i in table[dependent_var]["values"]:
-                        output += f"\t\tif self.{dependent_var} == '{i}':\n"
-                        output += f"\t\t\tself.{key} = fake.first_name_{i}()\n"
-                    return output
-                else:
-                    dependent_var = value["depends_on"][0]
-                    for i in table[dependent_var]["values"]:
-                        org_as_key = i.lower().replace(' ', '_')
-                        titles_by_org = table["titles_by_org"]["values"][org_as_key]
-                        output += f"\t\tif self.{dependent_var} == '{i}':\n"
-                        output += f"\t\t\tself.{key} = self.random_item(population={titles_by_org})\n"
-                    return output
-            elif "foreign_key" in value:
-                return f"\t\tself.{key} == {value['foreign_key']}\n"
-            elif "dist" in value:
-                num = value["dist"]
-                return f"\t\tself.{key} = self.random_int({num['min']}, {num['max']})\n"
-            elif "words" in value:
-                max_words = value["words"]["max"]
-                return f"\t\tself.{key} = fake.sentence(nb_words={max_words})\n"
-            # else:
-            #     return f"\t\tself.{key} = ##FILL ME INNNNN\n"
-            #     return f"{key}, {value}"
+        if value["type"] == "datetime" and "min_date" in value:
+            date = value["min_date"]
+            return f"\t\tself.{key} = self.created_at(datetime.datetime({date['year']}, {date['month']}, {date['day']}))\n"
+        elif all(k in value for k in ("values", "distribution")):
+            return f"\t\tself.{key} = self.random_item(population={value['values']}, distribution={value['distribution']})\n"
+        elif "values" in value:
+            return f"\t\tself.{key} = self.random_item(population={value['values']})\n"
+        elif key == "last_name":
+            return f"\t\tself.{key} = fake.last_name_nonbinary()\n"
+        elif key == "email" and "domain" in value:
+            return f"\t\tself.{key} = f'{{self.first_name.lower()}}{{self.last_name.lower()}}@{value['domain']}'\n"
+        elif key == "email" and "domain" not in value:
+            return f"\t\tself.{key} = f'{{self.first_name.lower()}}{{self.last_name.lower()}}@{{fake.safe_domain_name()}}'\n"
+        elif "depends_on" in value:
+            # single dependency
+            if len(value["depends_on"]) == 1 and value["depends_on"][0] == "gender":
+                dependent_var = value["depends_on"][0]
+                for i in table[dependent_var]["values"]:
+                    output += f"\t\tif self.{dependent_var} == '{i}':\n"
+                    output += f"\t\t\tself.{key} = fake.first_name_{i}()\n"
+                return output
+            elif len(value["depends_on"]) == 1 and "mapping" in value:
+                dependent_var = value["depends_on"][0]
+                for i in table[dependent_var]["values"]:
+                    output += f"\t\tif self.{dependent_var} == '{i}':\n"
+                    output += f"\t\t\tself.{key} = self.random_item(population={table[key]['mapping'][i]})\n"
+                return output
+        elif "foreign_key" in value:
+            return f"\t\tself.{key} == {value['foreign_key']}\n"
+        elif "dist" in value:
+            num = value["dist"]
+            return f"\t\tself.{key} = self.random_int({num['min']}, {num['max']})\n"
+        elif "words" in value:
+            max_words = value["words"]["max"]
+            return f"\t\tself.{key} = fake.sentence(nb_words={max_words})\n"
+        # else:
+        #     return f"\t\tself.{key} = ##FILL ME INNNNN\n"
+        #     return f"{key}, {value}"
 
 
 env = Environment(loader=file_loader, extensions=['jinja2.ext.do'])
@@ -114,7 +111,7 @@ template = env.get_template("wow_template.py.jinja2")
 template.globals['Table'] = Table()
 
 # read yaml file
-with open("yaml/org_chart.yaml", "r") as val:
+with open("yaml/job_listings.yaml", "r") as val:
     values = yaml.safe_load(val)
     output = template.render(tables=values)
     # write python file
--- prev_roles.py
+++ prev_roles.py
@@ -9,39 +9,17 @@ titles_by_org = {
     "customer_success": ["CSM", "CSE", "TSM"],
 }
 
-# employee_data = csv.DictReader(open("data/employees.csv"))
 today = datetime.date.today()
-# for row in employee_data:
-#     # print({"000 row": row})
-#     days_since_hired = (today -
-#                         (datetime.datetime.strptime(
-#                             row["hired_date"], '%Y-%m-%d').date())).days
 
-#     if days_since_hired / 365 > 1:
-#         row["title"] = random.choices(
-#             titles_by_org[row["org"].lower().replace(' ', '_')])[0]
-#         # print({"111 row": row})
-#         csv.DictWriter.writerow(employee_data, row)
+with open('data/employees.csv', 'r+', newline='') as obj:
+    dict_reader = csv.DictReader(obj)
+    dict_writer = csv.writer(obj)
 
-with open('data/employees.csv', 'a', newline='') as obj:
-    employee_data = csv.DictReader(obj)
-    print(employee_data)
-    for row in employee_data:
-        print({"000 row": row})
+    for row in list(dict_reader):
         days_since_hired = (today -
                             (datetime.datetime.strptime(
                                 row["hired_date"], '%Y-%m-%d').date())).days
-        print({"000 days_since_hired": days_since_hired})
-
-        # if days_since_hired / 365 > 1:
-        #     row["title"] = random.choices(
-        #         titles_by_org[row["org"].lower().replace(' ', '_')])[0]
-        #     # print({"111 row": row})
-        #     csv.DictWriter.writerow(employee_data, row)
-        #     # Pass the CSV  file object to the Dictwriter() function
-        #     # Result - a DictWriter object
-        #     dictwriter_object = csv.DictWriter(employee_data)
-        #     # Pass the data in the dictionary as an argument into the writerow() function
-        #     dictwriter_object.writerow(row)
-        #     # Close the file object
-    employee_data.close()
+        if days_since_hired / 365 > 6:
+            row["title"] = random.choices(
+                [i for i in titles_by_org[row["org"].lower().replace(' ', '_')] if i != row["org"]])[0]
+            dict_writer.writerow(row.values())
